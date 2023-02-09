a17f910ad714d9da4f4a72f67adee2708de74432
Gabriel Mocelin <gmocelin.jobsity@owletcare.com>
2023-02-08 21:03:27 -0300

    link item detail view as half sheet

--- ArgyleSDK/LinkItemDetail/LinkItemDetailView.swift
+++ ArgyleSDK/LinkItemDetail/LinkItemDetailView.swift
@@ -11,11 +11,30 @@ struct LinkItemDetailView: View {
     let linkItem: LinkItem
 
     var body: some View {
+        VStack(spacing: 24) {
+            header
+            description
+            Spacer()
+        }
+        .padding()
+    }
+
+    var header: some View {
         HStack(spacing: 16) {
-            Image("")
-                .frame(width: 75, height: 75)
-                .background(Color.secondary)
-                .cornerRadius(8)
+            AsyncImage(url: URL(string: linkItem.logoURL ?? "")) { image in
+                image
+                    .resizable()
+                    .cornerRadius(8)
+            } placeholder: {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color(uiColor: .secondarySystemBackground))
+                    .overlay {
+                        Image(systemName: "photo")
+                            .tint(Color(uiColor: .label))
+                            .frame(width: 25, height: 25)
+                    }
+            }
+            .frame(width: 75, height: 75)
 
             VStack(alignment: .leading, spacing: 8) {
                 Text(linkItem.name)
@@ -27,7 +46,11 @@ struct LinkItemDetailView: View {
 
             Spacer()
         }
-        .padding()
+        .padding(.vertical)
+    }
+
+    var description: some View {
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
     }
 }
 
